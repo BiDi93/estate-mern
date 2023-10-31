17cb6a8d9d7625243d86801261711f43dc3484c6
BiDi93 <abdmin9994061@gmail.com>
2023-10-31 15:13:19 +0800

    create api user test route in controller file

--- api/index.js
+++ api/index.js
@@ -1,22 +1,28 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import mongoose from 'mongoose'
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import UserRouter from "./routes/user-routes.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.mongoConnect).then(() => {
-  console.log("Connect to MONGODB")
-}).catch((err) => {
-  console.log(err)
-})
+mongoose
+  .connect(process.env.mongoConnect)
+  .then(() => {
+    console.log("Connect to MONGODB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.use("/api/user", UserRouter);
