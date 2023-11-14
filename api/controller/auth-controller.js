7eceb8f108a129af25756e53440171669d11f6a3
BiDi93 <abdmin9994061@gmail.com>
2023-11-14 12:38:39 +0800

    create sign in functionality

--- api/controller/auth-controller.js
+++ api/controller/auth-controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user-model.js";
 import bcrypt from "bcrypt";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -9,6 +10,29 @@ export const signup = async (req, res, next) => {
   try {
     await user1.save();
     res.status(201).json("User Has Been Created Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUserMail = await User.findOne({ email });
+    if (!validUserMail) return next(errorHandler(404, "User Not Found"));
+    const validPassword = await bcrypt.compare(
+      password,
+      validUserMail.get("password")
+    );
+    if (validPassword === false) {
+      return next(errorHandler(401, "Wrong Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUserMail._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
