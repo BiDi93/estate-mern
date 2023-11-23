79d2b7bbc2a818631bdbe9ae7f94d77908e11276
BiDi93 <abdmin9994061@gmail.com>
2023-11-24 00:52:48 +0800

    done image upload func

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [formData, setFormData] = useState({});
+  const [filePerc, setFilePerc] = useState(0);
+  const fileRef = useRef(null);
+  const [fileError, setFileError] = useState(false);
+  // console.log(filePerc);
+  // console.log(file);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
   return (
     <div className="p-3 max-w-lg mx-auto ">
       <h1 className="text-3xl font-semibold text-center my-7">PROFILE</h1>
       <form className="flex flex-col gap-3">
         <img
-          src={currentUser.avatar}
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
           className="rounded-full h-20 w-20 object-cover cursor-pointer self-center mt-2"
+        />
+        <p className="text-sm self-center">
+          {fileError ? (
+            <span className="text-red-700">Error image upload</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-500">{`Uploading in progress ${filePerc}%`}</span>
+          ) : filePerc === 100 && fileError === false ? (
+            <span className="text-green-500">Upload success</span>
+          ) : (
+            ""
+          )}
+        </p>
+
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          hidden
+          ref={fileRef}
+          accept="image/*"
         />
         <input
           type="text"
