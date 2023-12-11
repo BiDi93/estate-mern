9b993e50e48a092ecd560ecc5eefb99e8c767efa
BiDi93 <abdmin9994061@gmail.com>
2023-12-11 14:51:56 +0800

    complete update listing API route

--- api/routes/listing-routes.js
+++ api/routes/listing-routes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing } from "../controller/listing-controller.js";
-import { deleteListing } from "../controller/listing-controller.js";
+import {
+  createListing,
+  updateListing,
+  deleteListing,
+} from "../controller/listing-controller.js";
 
 const listingRouter = express.Router();
 
 listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete-listing/:id", verifyToken, deleteListing);
+listingRouter.post("/update-listing/:id", verifyToken, updateListing);
 
 export default listingRouter;
